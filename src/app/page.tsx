fae4a5835b5b255090bc961ac626e05508c0c989
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-12-07 20:07:25 -0300

    fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <h1>Hello</h1>;
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch("https://api.github.com/users/GuilhermeFRocha");
+  const todo = await response.json();
+
+  return <pre>{JSON.stringify(todo, null, 2)}</pre>;
 }
