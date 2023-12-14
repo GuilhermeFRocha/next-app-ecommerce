606a61088eef6fc6253ec30f752dc73dfaeab58e
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-12-13 21:16:21 -0300

    create context

--- src/contexts/card-context.tsx
+++ src/contexts/card-context.tsx
@@ -1,9 +1,27 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
-interface CardContextProps {}
+interface CardItem {
+  productId: string;
+  quantity: number;
+}
+
+interface CardContextProps {
+  items: CardItem[];
+  addToCart: (productId: string) => void;
+}
 
 const CardContext = createContext({} as CardContextProps);
 
 export function CardProvider({ children }: { children: React.ReactNode }) {
-  return <CardContext.Provider value={{}}>{children}</CardContext.Provider>;
+  const [cardItems, setCardItems] = useState<CardItem[]>([]);
+
+  function addToCart(productId: string) {}
+
+  return (
+    <CardContext.Provider value={{ items: cardItems, addToCart }}>
+      {children}
+    </CardContext.Provider>
+  );
 }
+
+export const useCard = () => useContext(CardContext);
